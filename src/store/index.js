0e7dfafa36101ce03047a08c68c318c60c03615b
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-04-17 20:47:16 -0300

    Configure dataSource in Vuex Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from "vuex";
+import dataSource from "@/data.json";
 
 export default createStore({
-  state: {},
+  state: dataSource,
   getters: {},
   mutations: {},
   actions: {},

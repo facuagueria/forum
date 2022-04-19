81f68f9dca0df2bf392e84d3d640baf6ecc024e7
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-04-19 12:45:17 -0300

    Modify createPost action

--- src/store/index.js
+++ src/store/index.js
@@ -43,10 +43,12 @@ export default createStore({
     },
   },
   actions: {
-    createPost(context, post) {
+    createPost({ commit, state }, post) {
       post.id = "gggg" + Math.random();
-      context.commit("setPost", { post });
-      context.commit("appendPostToThread", {
+      post.userId = state.authId;
+      post.publishedAt = Math.floor(Date.now() / 1000);
+      commit("setPost", { post });
+      commit("appendPostToThread", {
         postId: post.id,
         threadId: post.threadId,
       });

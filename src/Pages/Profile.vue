8664eda941b1f26d31fdb3926579f158b7e5a846
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-04-19 11:48:59 -0300

    Import new components in profile page

--- src/Pages/Profile.vue
+++ src/Pages/Profile.vue
@@ -2,36 +2,8 @@
   <div class="container">
     <div class="flex-grid">
       <div class="col-3 push-top">
-        <div class="profile-card">
-          <p class="text-center">
-            <img
-              :src="authUser.avatar"
-              :alt="`${authUser.name} profile picture`"
-              class="avatar-xlarge"
-            />
-          </p>
-
-          <h1 class="title">{{ authUser.username }}</h1>
-
-          <p class="text-lead">{{ authUser.name }}</p>
-
-          <p class="text-justify">{{ authUser.bio || "No bio specified" }}</p>
-
-          <span class="online">{{ authUser.username }} is online</span>
-
-          <div class="stats">
-            <span>{{ userPostsCount }} posts</span>
-            <span>{{ userThreadsCount }} threads</span>
-          </div>
-
-          <hr />
-
-          <p v-if="authUser.website" class="text-large text-center">
-            <i class="fa fa-globe"></i>
-            <a :href="authUser.website">{{ authUser.website }}</a>
-          </p>
-        </div>
-
+        <UserProfileCard :user="user" />
+        <UserProfileCardEditor :user="user" />
         <p class="text-xsmall text-faded text-center">
           Member since june 2003, last visited 4 hours ago
         </p>
@@ -52,7 +24,7 @@
 
         <hr />
 
-        <PostList :posts="userPosts" />
+        <PostList :posts="user.posts" />
       </div>
     </div>
   </div>
@@ -60,29 +32,15 @@
 
 <script>
 import PostList from "@/components/PostList";
+import UserProfileCard from "@/components/UserProfileCard";
+import UserProfileCardEditor from "@/components/UserProfileCardEditor";
 import { mapGetters } from "vuex";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Profile",
-  components: { PostList },
+  components: { PostList, UserProfileCard, UserProfileCardEditor },
   computed: {
-    ...mapGetters(["authUser"]),
-    userPosts() {
-      return this.$store.state.posts.filter(
-        (post) => post.userId === this.authUser.id
-      );
-    },
-    userPostsCount() {
-      return this.userPosts.length;
-    },
-    userThreads() {
-      return this.$store.state.threads.filter(
-        (thread) => thread.userId === this.authUser.id
-      );
-    },
-    userThreadsCount() {
-      return this.userThreads.length;
-    },
+    ...mapGetters({ user: "authUser" }),
   },
 };
 </script>
